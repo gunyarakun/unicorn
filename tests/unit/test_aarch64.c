0eb9feafab92cf700e2f94792ba36eb11ac8c8a7
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2018-02-09 19:18:54 -0800

    Update

--- tests/unit/test_aarch64.c
+++ tests/unit/test_aarch64.c
@@ -1,8 +1,3 @@
-/*
-    refer to issue #575.
-    to run correctly unicorn needs to be compiled for AArch64.
-*/
-
 #include "unicorn_test.h"
 #include <stdio.h>
 #include "unicorn/unicorn.h"
@@ -40,6 +35,10 @@ static int teardown(void **state)
     return 0;
 }
 
+/*
+    refer to issue #575.
+    to run correctly unicorn needs to be compiled for AArch64.
+*/
 void test_hang(void **state)
 {
     uint32_t code[] = {
@@ -92,10 +91,56 @@ void test_hang(void **state)
     printf("x1: %"PRIx64"\n", x1);
 }
 
+/* refer to issue #940 */
+void test_stp(void **state)
+{
+    uint32_t code[] = {
+        0xe61f00ad, /* STP Q6, Q6, [SP] */
+        0x6db923e9, /* STP D9, D8, [SP, #-112]! */
+    };
+
+    uc_engine *uc = *state;
+
+    uint64_t code_addr = 0x100000;
+    uint64_t stack_addr = 0x200000;
+
+    uc_mem_map(uc, code_addr, 0x1000, UC_PROT_ALL);
+    uc_mem_map(uc, stack_addr - 0x800, 0x1000, UC_PROT_ALL);
+
+    if (uc_mem_write(uc, code_addr, &code, sizeof(code))) {
+        printf("error on write\n");
+        return;
+    }
+
+    uint64_t quad[] = {
+        0x89abcdef01234567,
+        0xfeedfacedeadbeef,
+    };
+    uc_reg_write(uc, UC_ARM64_REG_Q6, quad);
+    uint64_t x = 0x08192a3b4c5d6e7f;
+    uc_reg_write(uc, UC_ARM64_REG_X8, &x);
+    x = 0x0f1e2d3c4b5a6978;
+    uc_reg_write(uc, UC_ARM64_REG_X9, &x);
+    x = stack_addr;
+    uc_reg_write(uc, UC_ARM64_REG_SP, &x);
+
+    uc_err err = uc_emu_start(uc, code_addr, code_addr + sizeof(code), 0, 0);
+    if (err) {
+        printf("error on start: %d\n", err);
+        return;
+    }
+
+    uc_mem_read(uc, stack_addr, (uint8_t *)&x, 8);
+    printf("1: %016llx", x);
+    uc_mem_read(uc, stack_addr + 8, (uint8_t *)&x, 8);
+    printf("2: %016llx", x);
+}
+
 int main(int argc, const char * argv[]) {
 
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(test_hang, init, teardown),
+        cmocka_unit_test_setup_teardown(test_stp, init, teardown),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);;
